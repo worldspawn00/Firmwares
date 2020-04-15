166a4547ddc233ff04e4451f867b4aedcafd68fd
worldspawn00 <mcmancuso@gmail.com>
2020-04-14 23:21:52 -0500

    fixing RGB

--- crkbd/keymaps/wsk/rules.mk
+++ crkbd/keymaps/wsk/rules.mk
@@ -8,3 +8,6 @@ SRC +=  ./lib/glcdfont.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
+
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = WS2812
