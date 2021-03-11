c9ac5964ab5a7373bfa2f2570fd71a0ba9de2b1b
worldspawn00 <mcmancuso@gmail.com>
2021-03-11 15:43:40 -0600

    fixing pin assignment

--- reviung30/reviung30.h
+++ reviung30/reviung30.h
@@ -26,17 +26,17 @@
  * represents the switch matrix.
  */
 #define LAYOUT_reviung30( \
-    K00, K01, K02, K03, K04,      K30, K31, K32, K33, K34, \
-    K10, K11, K12, K13, K14,      K40, K41, K42, K43, K44, \
-    K20, K21, K22, K23, K24,      K50, K51, K52, K53, K54, \
-                             K60 \
+    K01, K02, K03, K04, K05,      K30, K31, K32, K33, K34, \
+    K11, K12, K13, K14, K15,      K40, K41, K42, K43, K44, \
+    K21, K22, K23, K24, K25,      K50, K51, K52, K53, K54, \
+                             K61 \
 ) \
 { \
-    { K00, K01, K02, K03, K04 }, \
-    { K10, K11, K12, K13, K14 }, \
-    { K20, K21, K22, K23, K24 }, \
-    { K30, K31, K32, K33, K34 }, \
-    { K40, K41, K42, K43, K44 }, \
-    { K50, K51, K52, K53, K54 }, \
-    { K60, KC_NO, KC_NO, KC_NO, KC_NO } \
+    { KC_NO, K01, K02, K03, K04, K05 }, \
+    { KC_NO, K11, K12, K13, K14, K15 }, \
+    { KC_NO, K21, K22, K23, K24, K25 }, \
+    { K30, K31, K32, K33, K34, KC_NO }, \
+    { K40, K41, K42, K43, K44, KC_NO }, \
+    { K50, K51, K52, K53, K54, KC_NO }, \
+    { KC_NO, K61, KC_NO, KC_NO, KC_NO, KC_NO } \
 }
