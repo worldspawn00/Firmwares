e5f42d302e8af04b62858182d1e655f4e1e414c0
worldspawn00 <mcmancuso@gmail.com>
2024-09-23 19:23:18 -0500

    Delete TG4x/vial directory

--- TG4x/vial/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define RGB_MATRIX_LED_COUNT 7
-#define VIAL_KEYBOARD_UID {0xAE, 0xED, 0x87, 0x46, 0xC9, 0x6F, 0x59, 0x6B}
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 7
-#define RGB_DI_PIN D2
